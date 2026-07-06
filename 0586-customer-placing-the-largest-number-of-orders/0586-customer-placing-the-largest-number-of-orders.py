9d565a2e4ff1b63f9ea4bf2ef96f5f8ce52fbd4a
Saksham Agarwal <saksh2k05@gmail.com>
2026-07-07 05:12:35 +0530

    Time: 271 ms (86.09%), Space: 67.2 MB (45.67%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.py
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
-    counting = orders.groupby('customer_number').size().reset_index(name='count')
-    return counting.nlargest(1, 'count')[['customer_number']]
-    
+    counting = orders.groupby('customer_number').size()
+    counts = []
+    counts.append(counting.idxmax())
+    return pd.DataFrame({'customer_number': counts})
