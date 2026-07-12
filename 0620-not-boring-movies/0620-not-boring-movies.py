8a711428393d5776e2438b8451ba12149579081b
Saksham Agarwal <saksh2k05@gmail.com>
2026-07-12 22:10:54 +0530

    Time: 257 ms (80.27%), Space: 67.1 MB (85.46%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.py
+++ 0620-not-boring-movies/0620-not-boring-movies.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
-     cinema = cinema.sort_values('rating', ascending=False)
-     return cinema[(cinema['id']%2!=0) & (cinema['description']!='boring')]
+    cinema = cinema.sort_values('rating', ascending=False)
+    return cinema[(cinema['id']%2!=0) & (cinema['description']!='boring')]
